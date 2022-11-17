2f2229af3f6c2a829d2f60be633222d9e990a3cc
EPainsworth <Eliaspapa26@hotmail.com>
2022-11-17 09:28:01 +0100

    Push voor merge conflict.

--- header.php
+++ header.php
@@ -49,8 +49,8 @@ $databaseConnection = connectToDatabase();
 <!-- code voor US3: zoeken -->
         <ul id="ul-class-navigation">
             <li>
-                <a href="cart.php" class="HrefDecoration"> Cart</a>
                 <a href="browse.php" class="HrefDecoration"><i class="fas fa-search search"></i> Zoeken</a>
+                <a href="cart.php" class="HrefDecoration"><img class="cart-image" src="Public/Img/cart.png"></a>
             </li>
         </ul>
 <!-- einde code voor US3 zoeken -->
