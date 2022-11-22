a4953fa4236f81cef3fc63ce17277ba025df4add
EPainsworth <Eliaspapa26@hotmail.com>
2022-11-22 09:39:46 +0100

    Winkelwagen push

--- cart.php
+++ cart.php
@@ -1,7 +1,7 @@
 <?php
 include __DIR__ . "/header.php";
 include "cartfuncties.php";
-$totaalprijs = 0;
+$totaalPrijs = 0;
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -19,8 +19,9 @@ $totaalprijs = 0;
             <td>Image:</td>
             <td>Name:</td>
             <td>Quantity:</td>
-            <td>Price:</td>
+            <td>Price(incl. btw):</td>
             <td>ID:</td>
+            <td></td>
         </tr>
         </thead>
         <tbody class="bodycontainer">
@@ -29,14 +30,15 @@ $totaalprijs = 0;
         foreach ($cart as $nr => $aantal):
             $stockItem = getStockItem($nr, $databaseConnection);
             $stockItemImage = getStockItemImage($nr, $databaseConnection);
-            $totaalprijs += $aantal * $stockItem['SellPrice'];
-            if (ISSET($_GET[$nr])) {
-                addProductToCart($nr);
-                unset($_GET[$nr]);
+            $totaalPrijs += $aantal * $stockItem['SellPrice'];
+            if (ISSET($_GET["del".$nr])) {
+                deleteProductFromCart($nr);
+                unset($_GET["del".$nr]);
+                header("Refresh:0");
             }
-            if (ISSET($_GET[$nr."-"])) {
-                removeProductFromCart($nr);
-                unset($_GET[$nr."-"]);
+            if (ISSET($_POST["quantity".$nr])) {
+                setProductInCart($nr,$_POST["quantity".$nr]);
+                header("Refresh:0");
             }
             ?>
             <tr class="data">
@@ -44,9 +46,10 @@ $totaalprijs = 0;
                     <img src="Public/StockItemIMG/<?php if (!empty($stockItemImage) ? print($stockItemImage[0]['ImagePath']) : print 'error.png') ?>">
                 </td>
                 <td><h6><?= $stockItem['StockItemName'] ?></h6></td>
-                <td><h4><a href="cart.php?<?php echo $nr."-" ?>=false">-</a><?= $aantal ?><a href="cart.php?<?php echo $nr ?>=true">+</a></h4></td>
+                <td><div class="qty-container"><h4><form class="aantalform" action="cart.php" method="post"><input class="aantal" type="number" name="<?php echo "quantity".$nr?>" min="1" max="<?php $stockItem["QuantityOnHand"]?>" value="<?php echo (ISSET($_POST["quantity".$nr])) ? $_POST["quantity".$nr] :  $aantal?>" placeholder="<?php echo (ISSET($_POST["quantity".$nr])) ? $_POST["quantity".$nr] :  $aantal?>"><input class="aantal2" type="submit" value="OK"></form></h4></div></td>
                 <td><h4>€<?= number_format((float)$stockItem['SellPrice'], 2, '.', '') ?></h4></td>
-                <td><h4><a href="<?= print("view.php?id=" . $nr) ?>"><?php echo $nr ?></a></h4></td>
+                <td><h4><a href="view.php?id=<?= $nr ?>"><?php echo $nr ?></a></h4></td>
+                <td><h4><a href="cart.php?<?php echo "del".$nr?>">verwijderen</a> </h4></td>
             </tr>
 
         <?php
@@ -55,7 +58,7 @@ $totaalprijs = 0;
 
         </tbody>
     </table>
-    <h3>Totaalprijs: €<?= number_format((float)$totaalprijs, 2, '.', '') ?></h3>
+    <h3>Totaalprijs (incl. BTW): €<?= number_format((float)$totaalPrijs, 2, '.', '') ?></h3>
 </div>
 </body>
 </html>
