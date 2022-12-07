658f4ca8fad0d02fd1f17dba7cbfe6dc67ab92a3
thimo812 <s1182799@student.windesheim.nl>
2022-12-07 22:06:38 +0100

    .

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+include __DIR__ . "/header.php";
+include "cartfuncties.php";
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>register</title>
+    <link rel="stylesheet" href="Public/CSS/style.css">
+</head>
+<body>
+<div class="registerContainer">
+    <h1>register</h1>
+</div>
+</body>
+
