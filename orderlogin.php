49fb50ee9cd648cebe16ec202d13e074cd39ce18
thimo812 <s1182799@student.windesheim.nl>
2022-12-09 16:15:55 +0100

    .

---
+++ orderlogin.php
@@ -0,0 +1,25 @@
+<?php
+include __DIR__ . "/header.php";
+include "cartfuncties.php";
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>orderlogin</title>
+    <link rel="stylesheet" href="Public/CSS/cart.css">
+</head>
+<body>
+<h1 style="text-align: center;">Login</h1>
+<form method="post" action="checkout.php" class="container">
+<div class="loginField">
+    <input type="text" name="mail" placeholder="E-mail" required>
+    <input type="password" name="pword" placeholder="Password" required>
+</div>
+<div>
+    <button>Back to cart</button>
+    <input type="submit" value="Login" name="login">
+</div>
+</form>
+</body>
