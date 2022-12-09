9b15e14d594c74fee5520b01acd2309c754bb5dc
thimo812 <thimo8123@gmail.com>
2022-12-09 07:07:03 -0800

    register scherm gemaakt

--- register.php
+++ register.php
@@ -11,8 +11,37 @@ include "cartfuncties.php";
     <link rel="stylesheet" href="Public/CSS/style.css">
 </head>
 <body>
-<div class="registerContainer">
+<div class="registerTitle">
     <h1>register</h1>
+</div>
+<div class="container">
+<form method="post" action="confirmation.php">
+    <table>
+        <div class="invoer">
+            <input type="text" id="fname" name="fname" placeholder="First name" required>
+            <input type="text" id="lname" name="lname" placeholder="Last name" required>
+        </div>
+        <div class="invoer">
+            <input type="text" id="mail" name="mail" placeholder="E-mail" required>
+            <input type="text" id="phone" name="phone" placeholder="Phone number" required>
+        </div>
+        <div class="invoer">
+            <input type="text" id="pcode" name="pcode" placeholder="Postal code" required>
+            <input type="text" id="city" name="city" placeholder="City" required>
+        </div>
+        <div class="invoer">
+            <input type="text" id="hnumber" name="hnumber" placeholder="House number" required>
+            <input type="text" id="apartment" name="apartment" placeholder="Apartment/suite (opt.)">
+        </div>
+        <div class="invoer">
+            <input type="text" id="password" name="password" placeholder="Password" required>
+            <input type="text" id="cpassword" name="cpassword" placeholder="Confirm password">
+        </div>
+        <div class="invoer">
+            <input class="submitRegistration" type="submit" value="Continue" name="confirmRegistration">
+        </div>
+    </table>
+</form>
 </div>
 </body>
 
