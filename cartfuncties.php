d5e69d8593343d9c5ba3dd3f801dedd6e8a00646
ruben <rubeneekhof@gmail.com>
2022-12-13 13:05:14 +0100

    fixed merge conflict

--- cartfuncties.php
+++ cartfuncties.php
@@ -1,27 +1,35 @@
 <?php
 
-
-if(!isset($_SESSION))
+if(!isset($_SESSION)) // altijd hiermee starten als je gebruik wilt maken van sessiegegevens
 {
     session_start();
-}                                // altijd hiermee starten als je gebruik wilt maken van sessiegegevens
-
-function getCart()
-{
-    if (isset($_SESSION['cart'])) {               //controleren of winkelmandje (=cart) al bestaat
-        $cart = $_SESSION['cart'];                  //zo ja:  ophalen
-    } else {
-        $cart = array();                            //zo nee: dan een nieuwe (nog lege) array
-    }
-    return $cart;                               // resulterend winkelmandje terug naar aanroeper functie
 }
 
-function saveCart($cart)
+/**
+ * Haal de cart op.
+ * @return array De cart.
+ */
+function getCart() : array
+{
+    return $_SESSION['cart'] ?? array();   // resulterend winkelmandje terug naar aanroeper functie
+}
+
+/**
+ * Sla de cart op in de sessie.
+ * @param $cart array De cart.
+ * @return void Deze functie heeft geen returnwaarde.
+ */
+function saveCart(array $cart) : void
 {
     $_SESSION["cart"] = $cart;                  // werk de "gedeelde" $_SESSION["cart"] bij met de meegestuurde gegevens
 }
 
-function addProductToCart($stockItemID)
+/**
+ * Voeg een product toe aan de cart.
+ * @param $stockItemID int Het ID van het product.
+ * @return void Deze functie heeft geen returnwaarde.
+ */
+function addProductToCart(int $stockItemID) : void
 {
     $cart = getCart();                          // eerst de huidige cart ophalen
 
@@ -34,7 +42,12 @@ function addProductToCart($stockItemID)
     saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
 }
 
-function removeProductFromCart($stockItemID)
+/**
+ * Verwijder een product uit de cart.
+ * @param $stockItemID int Het ID van het product.
+ * @return void Deze functie heeft geen returnwaarde.
+ */
+function removeProductFromCart(int $stockItemID) : void
 {
     $cart = getCart();                          // eerst de huidige cart ophalen
 
@@ -47,7 +60,12 @@ function removeProductFromCart($stockItemID)
     saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
 }
 
-function deleteProductFromCart($stockItemID)
+/**
+ * Verwijder een product uit de cart.
+ * @param $stockItemID int Het ID van het product.
+ * @return void Deze functie heeft geen returnwaarde.
+ */
+function deleteProductFromCart(int $stockItemID) : void
 {
     $cart = getCart();                          // eerst de huidige cart ophalen
 
@@ -58,7 +76,13 @@ function deleteProductFromCart($stockItemID)
     saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
 }
 
-function setProductInCart($stockItemID,$amount)
+/**
+ * Verander het aantal van een product in de cart.
+ * @param $stockItemID int Het ID van het product.
+ * @param $amount int Het nieuwe aantal.
+ * @return void Deze functie heeft geen returnwaarde.
+ */
+function setProductInCart(int $stockItemID, int $amount) : void
 {
     $cart = getCart();                          // eerst de huidige cart ophalen
     $cart[$stockItemID] = $amount;
@@ -66,7 +90,12 @@ function setProductInCart($stockItemID,$amount)
     saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
 }
 
-function clearCart() {
+/**
+ * Leeg de cart.
+ * @return void Deze functie heeft geen returnwaarde.
+ */
+function clearCart() : void
+{
     $cart = getCart();  // haal de huidige cart op
 
     foreach ($cart as $nr => $aantal) { // loop door de cart
@@ -74,6 +103,10 @@ function clearCart() {
     }
 }
 
+/**
+ * Kijkt of de cart leeg is.
+ * @return bool True als de cart leeg is, anders false.
+ */
 function isCardEmpty(): bool
 {
     $cart = getCart();  // haal de huidige cart op
@@ -82,4 +115,25 @@ function isCardEmpty(): bool
         return true; // als de cart leeg is, geef true terug
     }
     return false; // als de cart niet leeg is, geef false terug
+}
+
+/**
+ * Formatteert de inhoud van de cart als een string.
+ * @param $databaseConnection mysqli De database connectie.
+ * @return string De inhoud van de cart als een string.
+ */
+function toString(mysqli $databaseconnection): string
+{
+    $cart = getCart();  // haal de huidige cart op
+    $string = "";  // maak een lege string aan
+    foreach ($cart as $nr => $aantal) { // loop door de cart
+        // query de productnaam uit de database
+        $query = "SELECT StockItemName FROM stockitems WHERE StockItemID = $nr"; // maak een query die de productnaam ophaalt
+        $statement = mysqli_prepare($databaseconnection, $query); // maak een verklaring van de query
+        mysqli_stmt_execute($statement); // voer de query uit
+        $result = mysqli_stmt_get_result($statement); // haal het resultaat op
+        $row = mysqli_fetch_assoc($result); // haal de rij op
+        $string .= $row["StockItemName"] . " x " . $aantal . "<br>"; // voeg de productnaam en het aantal toe aan de string
+    }
+    return $string; // geef de string terug
 }
