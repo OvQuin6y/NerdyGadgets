0e82b898ca4835f251dbb8bf9e5d91fa352b4028
JoelleDoorn <s1183785@student.windesheim.nl>
2022-12-19 14:05:38 +0100

    US13 taal aanpassen

--- header.php
+++ header.php
@@ -72,7 +72,7 @@ $databaseConnection = connectToDatabase();
                             } ?>>Nederlands</option>
                         </select>
                     </form>
-                    <a href="browse.php" class="HrefDecoration"><i class="fas fa-search search"></i> Search</a>
+                    <a href="browse.php" class="HrefDecoration"><i class="fas fa-search search"></i><?php echo " " . getTranslation($databaseConnection, $lang, "Kop_zoeken")?></a>
                     <a href="cart.php" class="HrefDecoration"><img style="margin-right: 10px" class="Cart-Image" src="Public/Img/winkelwagen.png"></a>
                     <div class="dropdown">
                         <button class="account-button"><img style="margin-right: 10px" class="cart-image" src="Public/Img/account.png"><?php echo (ISSET($_SESSION["klantID"])) ? getName($databaseConnection, $_SESSION["klantID"]) : "Account"?></button>
