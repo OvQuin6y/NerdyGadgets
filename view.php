04340c3d4bace45c4a12f849ed56ceb08e1d7900
OvQuin6y <s1185994@student.windesheim.nl>
2022-12-19 12:37:21 +0100

    Review modal

--- view.php
+++ view.php
@@ -7,50 +7,10 @@ $StockItem = getStockItem($_GET['id'], $databaseConnection,$_SESSION["lang"]);
 $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
 ?>
 <style>
-    .rate{
-        border-bottom-right-radius: 12px;
-        border-bottom-left-radius: 12px;
-    }
 
-    .rating {
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: center
-    }
-
-    .rating>input {
-        display: none
-    }
-
-    .rating>label {
-        position: relative;
-        width: 1em;
-        font-size: 30px;
-        font-weight: 300;
-        color: #FFD600;
-        cursor: pointer
-    }
-
-    .rating>label::before {
-        content: "\2605";
-        position: absolute;
-        opacity: 0
-    }
-
-    .rating>label:hover:before,
-    .rating>label:hover~label:before {
-        opacity: 1 !important
-    }
-
-    .rating>input:checked~label:before {
-        opacity: 1
-    }
-
-    .rating:hover>input:checked~label:before {
-        opacity: 0.4
-    }
 </style>
 <link rel="stylesheet" href="Public/CSS/cart.css">
+<link rel="stylesheet" href="Public/CSS/review.css">
 <div id="CenteredContent">
     <?php
     if ($StockItem != null) {
@@ -97,7 +57,7 @@ $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
                       <span><i class="fa fa-map-marker stop mt-2"></i> <small class="text-truncate ml-2">Mandir Road, Mayur vihar, new delhi</small> </span>
                   </div>
                   <div class="rate bg-success py-3 text-white mt-3">
-                      <h6 class="mb-0">Rate the product</h6>
+                      <h6 class="mb-0">Rate this product</h6>
                       <div class="rating">
                           <input type="radio" name="rating" value="5" id="5"><label for="5">☆</label> <input type="radio" name="rating" value="4" id="4"><label for="4">☆</label> <input type="radio" name="rating" value="3" id="3"><label for="3">☆</label> <input type="radio" name="rating" value="2" id="2"><label for="2">☆</label> <input type="radio" name="rating" value="1" id="1"><label for="1">☆</label>
                       </div>
