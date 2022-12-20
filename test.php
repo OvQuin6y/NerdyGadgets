4452385dc7dcedc298e5062ffa835031dc20f8f9
ruben <rubeneekhof@gmail.com>
2022-12-20 09:25:17 +0100

    Implemented transaction

--- test.php
+++ test.php
@@ -1,21 +1,9 @@
 <?php
-include "database.php";
 
-$databaseConnection = connectToDatabase();
+$stockItemId = 5;
 
-function getPassword(mysqli $databaseConnection, $mail)
-{
-    $query = "
-                SELECT Password
-                FROM klant
-                WHERE Email ='" . $mail .  "';";
+echo "<a href='view.php?id=$stockItemId'>";
 
-    $result = $databaseConnection->query($query);
-    $return = "";
-    while ($row = $result->fetch_array()) {
-        $return = $row["Password"];
-    }
-    return $return;
-}
 
-echo getPassword($databaseConnection, "thimo8123@gmail.com");
+
+
